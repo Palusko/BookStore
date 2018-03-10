345c73fe675be6f90ac2bc0add5bd1bb277c71da
Pavol Almasi <pavol_the_best@hotmail.com>
2018-03-09 23:19:54 -0500

    Added Genre layouts and controller

--- BookStore/Controllers/GenreController.cs
+++ BookStore/Controllers/GenreController.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using BookStore.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -8,10 +10,36 @@ namespace BookStore.Controllers
 {
     public class GenreController : Controller
     {
+        private ApplicationDbContext db;
+        public GenreController()
+        {
+            db = new ApplicationDbContext();
+        }
+
         // GET: Genre
         public ActionResult Index()
         {
+            //var genres = db.Genres;
+            return View(db.Genres.ToList());
+        }
+
+        /*
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create()
+        {            
+            return View("Create");
+        }
+        */
+                
+        public ActionResult Create()
+        {
             return View();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
         }
     }
 }
